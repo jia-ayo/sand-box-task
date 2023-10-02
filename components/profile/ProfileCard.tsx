c1ff222bc886ac2c50d89f6d717b715bcd08507c
jia-ayo <milkjia11@gmail.com>
2023-10-02 17:37:03 +0100

    profile card

--- components/profile/ProfileCard.tsx
+++ components/profile/ProfileCard.tsx
@@ -10,7 +10,7 @@ const ProfileCard:React.FC<ProfileCardProps> = ({name, username, profileImage, d
   
   return (
     <div>
-      <div className="border border-solid border-gray-700 mx-8 px-2 m-2 p-2 grid grid-cols-2 rounded-xl">
+      <div className="border border-solid border-gray-700 mx-8 px-2 m-2 p-2 flex flex-col md:grid md:grid-cols-2 rounded-xl">
         <div className="flex justify-center items-center">
           <div className="h-1/3 w-1/3 flex items-center justify-center">
             <img
