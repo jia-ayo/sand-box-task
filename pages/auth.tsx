46851a9fa6174d024216a5b354f1a4319d6ff364
jia-ayo <milkjia11@gmail.com>
2023-10-02 15:39:00 +0100

    leaves auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,19 @@
 import Login from "@/components/Authen/Login";
 import Register from "@/components/Authen/Register";
-import React, { useState } from "react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { Router, useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 export default function Auth() {
+  const router = useRouter();
+    const { data: currentUser } = useCurrentUser();
   const [showLogin, setShowLogin] = useState(true);
+
+   useEffect(() => {
+     if (currentUser) {
+       router.push("/");
+     }
+   }, [currentUser, router]);
 
   return (
     <div className="flex items-center justify-center min-h-screen">
